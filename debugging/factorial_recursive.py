cbdd1391bc6aaa7ef7541b51359c868d6d58228b
Mollydayne <cl.w.perez@gmail.com>
2025-01-13 10:51:32 +0100

    Changing factorial recursive file

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -3,19 +3,21 @@ import sys
 
 def factorial(n):
     """
-    Calculate the factorial of a non-negative integer using recursion.
+    Calculate the factorial of a given non-negative integer.
 
     Parameters:
-    n (int): The non-negative integer for which the factorial is to be calculated.
+    n (int): A non-negative integer for which the factorial is to be calculated.
+             The function assumes n is a valid non-negative integer, as negative
+             integers do not have a factorial.
 
     Returns:
-    int: The factorial of the input number. If n is 0, returns 1.
+    int: The factorial of the input integer n.
+         If n is 0, the function returns 1, since 0! is defined as 1.
     """
     if n == 0:
         return 1
     else:
         return n * factorial(n-1)
 
-# Get the input number from the command-line argument
 f = factorial(int(sys.argv[1]))
 print(f)
